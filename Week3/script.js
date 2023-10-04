4cd61b15c0ea2b6028b83a05cf3d7f6719d2add4
TheGirdy <145339115+TheGirdy@users.noreply.github.com>
2023-10-04 19:54:26 +0100

    Theory from past lecture
    Wanted to test Javascript so I used some theory from a past lecture.

---
+++ Week3/script.js
@@ -0,0 +1,20 @@
+function getHue(range) {
+    var c = function () { return Math.floor(Math.random()*range); }
+    return "rgb(" + c() + "," + c() + "," + c() + ")";
+  }
+  function getTable(size, range) {
+    var s = "<table>";
+    for (var i=0; i<size; i++) {
+      s += "<tr height='10px'>";
+      for (var j=0; j<size; j++)
+      s += "<td width='10px' style=\"background-color:" + getHue(range) +
+      "\">" + "     </td>";
+      s += "</tr>";
+    }
+    s += "</table>";
+  return s;
+  }
+  
+ /* var s = getTable(Math.floor(Math.random()*40 + 1), 256);
+  document.writeln(s); */
+  
